b802d740aa33f8550047a152d437cadf8c55b0fa
apuente93 <apuente@wisc.edu>
2014-02-19 12:03:40 -0600

    Added Bootstrap gem and modified layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,4 +9,36 @@ describe "Static pages" do
       expect(page).to have_content('Lazy Hub')
     end
   end
+
+  describe "Laugh page" do
+
+    it "should have the content 'Laugh'" do
+      visit '/static_pages/laugh'
+      expect(page).to have_content('Laugh')
+    end
+  end
+
+  describe "Game page" do
+
+    it "should have the content 'Game'" do
+      visit '/static_pages/game'
+      expect(page).to have_content('Game')
+    end
+  end
+
+  describe "Community page" do
+
+    it "should have the content 'Community'" do
+      visit '/static_pages/community'
+      expect(page).to have_content('Community')
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
 end
