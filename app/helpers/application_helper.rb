45e4a173b648a3e7bb2ac6f0e24ffa380838eddf
apuente93 <apuente@wisc.edu>
2014-02-21 15:56:31 -0600

    Changed main title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Lazy Hub"
+    base_title = "Lazy Hub - the entertainment system for the web"
     if page_title.empty?
       base_title
     else
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,5 @@
 def full_title(page_title)
-  base_title = "Lazy Hub"
+  base_title = "Lazy Hub - the entertainment system for the web"
   if page_title.empty?
     base_title
   else
