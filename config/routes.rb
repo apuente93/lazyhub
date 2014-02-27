8ffd5587bac82ef2b9eea2a8b9f0ac3056a37797
apuente93 <apuente@wisc.edu>
2014-02-27 11:46:08 -0600

    Added a route to users, and created a user show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Lazyhub::Application.routes.draw do
+  resources :users
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/game',    to: 'static_pages#game',    via: 'get'
