121585855838ba3a9b891248ffd0b10d94a657d2
apuente93 <apuente@wisc.edu>
2014-04-08 11:45:42 -0500

    added age to users, so that I can later on filter content by age group

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140316205847) do
+ActiveRecord::Schema.define(version: 20140408164438) do
 
   create_table "articles", force: true do |t|
     t.integer  "upvote"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140316205847) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.integer  "age"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
