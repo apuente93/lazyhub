6b6400975fe8b32a77d9d69bb417c6888d40e3d3
apuente93 <apuente@wisc.edu>
2014-05-20 15:55:57 -0500

    users can now have microposts, manipulating microposts in progress

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513192020) do
+ActiveRecord::Schema.define(version: 20140520195324) do
 
   create_table "articles", force: true do |t|
     t.integer  "upvote"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 20140513192020) do
     t.string   "article_type"
     t.integer  "article_type_id"
   end
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
 
   create_table "users", force: true do |t|
     t.string   "name"
