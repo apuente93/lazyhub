98f439a0c524eb2fd07fee3677c755a75691dbf1
apuente93 <apuente@wisc.edu>
2014-06-20 14:38:26 -0500

    added pagination for articles (after 20)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,26 +1,26 @@
 class StaticPagesController < ApplicationController
   def home
-    @articles = Article.all
+    @articles = Article.paginate(:page => params[:page], :per_page => 20)
     @articles.sort_by! { |a| [-a.get_upvotes.size, -a.views]}
   end
 
   def laugh
-    @articles = Article.where(article_type_id: 1)
+    @articles = Article.where(article_type_id: 1).paginate(:page => params[:page], :per_page => 20)
     @articles.sort_by! { |a| [-a.get_upvotes.size, -a.views]}
   end
 
   def game
-    @articles = Article.where(article_type_id: 2)
+    @articles = Article.where(article_type_id: 2).paginate(:page => params[:page], :per_page => 20)
     @articles.sort_by! { |a| [-a.get_upvotes.size, -a.views]}
   end
 
   def news
-    @articles = Article.where(article_type_id: 3)
+    @articles = Article.where(article_type_id: 3).paginate(:page => params[:page], :per_page => 20)
     @articles.sort_by! { |a| [-a.get_upvotes.size, -a.views]}
   end
 
   def random
-    @articles = Article.where(article_type_id: 4)
+    @articles = Article.where(article_type_id: 4).paginate(:page => params[:page], :per_page => 20)
     @articles.sort_by! { |a| [-a.get_upvotes.size, -a.views]}
   end
 
