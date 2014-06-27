4d79e907833cd3f2f282e001bdcf4e480628a51e
apuente93 <apuente@wisc.edu>
2014-06-27 00:46:00 -0500

    deleted shit

--- db/migrate/20140513192020_add_downvoted_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddDownvotedToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :downvoted, :boolean, default: false
-  end
-end
--- db/migrate/20140627051321_add_upvote_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddUpvoteToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :upvote, :integer
-  end
-end
