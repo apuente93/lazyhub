e9257f4611d1f170f6ea4d69aa1565e5d9c1dfec
apuente93 <apuente@wisc.edu>
2014-06-27 02:49:05 -0500

    finally fixed pagination bug, where ordering was not working. Now we have pagination with ordering

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,27 +1,22 @@
 class StaticPagesController < ApplicationController
   def home
-    @articles = Article.all
-    @articles.sort_by! { |a| [-a.get_upvotes.size, -a.views]}
+    @articles = Article.all.sort_by! { |a| [-a.get_upvotes.size, -a.views]}.paginate(:page => params[:page], :per_page => 20)
   end
 
   def laugh
-    @articles = Article.where(article_type_id: 1)
-    @articles.sort_by! { |a| [-a.get_upvotes.size, -a.views]}
+    @articles = Article.where(article_type_id: 1).sort_by! { |a| [-a.get_upvotes.size, -a.views]}.paginate(:page => params[:page], :per_page => 20)
   end
 
   def game
-    @articles = Article.where(article_type_id: 2)
-    @articles.sort_by! { |a| [-a.get_upvotes.size, -a.views]}
+    @articles = Article.where(article_type_id: 2).sort_by! { |a| [-a.get_upvotes.size, -a.views]}.paginate(:page => params[:page], :per_page => 20)
   end
 
   def news
-    @articles = Article.where(article_type_id: 3)
-    @articles.sort_by! { |a| [-a.get_upvotes.size, -a.views]}
+    @articles = Article.where(article_type_id: 3).sort_by! { |a| [-a.get_upvotes.size, -a.views]}.paginate(:page => params[:page], :per_page => 20)
   end
 
   def random
-    @articles = Article.where(article_type_id: 4)
-    @articles.sort_by! { |a| [-a.get_upvotes.size, -a.views]}
+    @articles = Article.where(article_type_id: 4).sort_by! { |a| [-a.get_upvotes.size, -a.views]}.paginate(:page => params[:page], :per_page => 20)
   end
 
   def about
