e7218c439be971d051c7e248a35d6b0cf42673e3
apuente93 <apuente@wisc.edu>
2014-11-18 00:00:08 -0600

    minor css changes to table 1. Also fixed crash when moving from home page to other static pages.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,18 +6,22 @@ class StaticPagesController < ApplicationController
 
   def laugh
     @articles = Article.where(article_type_id: 1).sort_by! { |a| [-a.get_upvotes.size, -a.views]}.paginate(:page => params[:page], :per_page => 20)
+    @top_articles = Article.all.sort_by! { |a| a.rank }
   end
 
   def game
     @articles = Article.where(article_type_id: 2).sort_by! { |a| [-a.get_upvotes.size, -a.views]}.paginate(:page => params[:page], :per_page => 20)
+    @top_articles = Article.all.sort_by! { |a| a.rank }
   end
 
   def news
     @articles = Article.where(article_type_id: 3).sort_by! { |a| [-a.get_upvotes.size, -a.views]}.paginate(:page => params[:page], :per_page => 20)
+    @top_articles = Article.all.sort_by! { |a| a.rank }
   end
 
   def random
     @articles = Article.where(article_type_id: 4).sort_by! { |a| [-a.get_upvotes.size, -a.views]}.paginate(:page => params[:page], :per_page => 20)
+    @top_articles = Article.all.sort_by! { |a| a.rank }
   end
 
   def about
