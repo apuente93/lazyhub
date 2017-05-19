6089ecf3f613392c070a5e9c4e0779c8388ab132
apuente93 <apuente@wisc.edu>
2017-05-18 21:46:54 -0600

    fixed issue with assets not loading on page load in development

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(:default, Rails.env)
 
 module Lazyhub
   class Application < Rails::Application
-    config.autoload_paths += %W(#{config.root}/lib)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -21,7 +20,5 @@ module Lazyhub
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     I18n.enforce_available_locales = false
-    
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
