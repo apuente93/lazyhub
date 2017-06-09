4ab86231802eb1574301e3652d0edb557694a6eb
apuente93 <apuente@wisc.edu>
2017-06-09 00:46:26 -0600

    Finally finished performance upgrades. Initial load page decreased from 8.5 secs to 2 secs

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,63 +5,63 @@ class StaticPagesController < ApplicationController
     @page = "home"
     @articles = Article.all.paginate(:page => params[:page], :per_page => 20)
 
-    @top_laugh = @articles.where(article_type: "Laugh").sort_by! { |a| [-a.get_upvotes.size]}[0]
-    @top_game = @articles.where(article_type: "Game").sort_by! { |a| [-a.get_upvotes.size]}[0]
-    @top_news = @articles.where(article_type: "News").sort_by! { |a| [-a.get_upvotes.size]}[0]
-    @top_tech = @articles.where(article_type: "Tech").sort_by! { |a| [-a.get_upvotes.size]}[0]
-    @top_social = @articles.where(article_type: "Social").sort_by! { |a| [-a.get_upvotes.size]}[0]
-    @top_sports = @articles.where(article_type: "Sports").sort_by! { |a| [-a.get_upvotes.size]}[0]
-    @top_random = @articles.where(article_type: "Random").sort_by! { |a| [-a.get_upvotes.size]}[0]
+    @top_laugh = @articles.where(article_type: "Laugh")[0]
+    @top_game = @articles.where(article_type: "Game")[0]
+    @top_news = @articles.where(article_type: "News")[0]
+    @top_tech = @articles.where(article_type: "Tech")[0]
+    @top_social = @articles.where(article_type: "Social")[0]
+    @top_sports = @articles.where(article_type: "Sports")[0]
+    @top_random = @articles.where(article_type: "Random")[0]
     @top_articles = [@top_laugh, @top_game, @top_news, @top_tech, @top_social, @top_sports, @top_random]
   end
 
   def laugh
     @page = "laugh"
     @subscription = Subscription.new
-    @articles = Article.where(article_type: "Laugh").sort_by! { |a| [-a.get_upvotes.size]}.paginate(:page => params[:page], :per_page => 20)
-    @top_articles = Article.where(article_type: "Laugh").sort_by! { |a| [-a.get_upvotes.size]}.take(5)
+    @articles = Article.where(article_type: "Laugh").paginate(:page => params[:page], :per_page => 20)
+    @top_articles = Article.where(article_type: "Laugh").take(5)
   end
 
   def game
     @page = "game"
     @subscription = Subscription.new
-    @articles = Article.where(article_type: "Game").sort_by! { |a| [a.isOld ? 1 : 0, -a.get_upvotes.size, -a.views, -a.created_at.to_i, -a.id]}.paginate(:page => params[:page], :per_page => 20)
-    @top_articles = Article.where(article_type: "Game").sort_by! { |a| [-a.get_upvotes.size, -a.views, -a.id]}.take(5)
+    @articles = Article.where(article_type: "Game").paginate(:page => params[:page], :per_page => 20)
+    @top_articles = Article.where(article_type: "Game").take(5)
   end
 
   def news
     @page = "news"
     @subscription = Subscription.new
-    @articles = Article.where(article_type: "News").sort_by! { |a| [a.isOld ? 1 : 0, -a.get_upvotes.size, -a.views, -a.created_at.to_i, -a.id]}.paginate(:page => params[:page], :per_page => 20)
-    @top_articles = Article.where(article_type: "News").sort_by! { |a| [-a.get_upvotes.size, -a.views, -a.id]}.take(5)
+    @articles = Article.where(article_type: "News").paginate(:page => params[:page], :per_page => 20)
+    @top_articles = Article.where(article_type: "News").take(5)
   end
 
   def tech
     @page = "tech"
     @subscription = Subscription.new
-    @articles = Article.where(article_type: "Tech").sort_by! { |a| [a.isOld ? 1 : 0, -a.get_upvotes.size, -a.views, -a.created_at.to_i, -a.id]}.paginate(:page => params[:page], :per_page => 20)
-    @top_articles = Article.where(article_type: "Tech").sort_by! { |a| [-a.get_upvotes.size, -a.views, -a.id]}.take(5)
+    @articles = Article.where(article_type: "Tech").paginate(:page => params[:page], :per_page => 20)
+    @top_articles = Article.where(article_type: "Tech").take(5)
   end
 
   def social
     @page = "social"
     @subscription = Subscription.new
-    @articles = Article.where(article_type: "Social").sort_by! { |a| [a.isOld ? 1 : 0, -a.get_upvotes.size, -a.views, -a.created_at.to_i, -a.id]}.paginate(:page => params[:page], :per_page => 20)
-    @top_articles = Article.where(article_type: "Social").sort_by! { |a| [-a.get_upvotes.size, -a.views, -a.id]}.take(5)
+    @articles = Article.where(article_type: "Social").paginate(:page => params[:page], :per_page => 20)
+    @top_articles = Article.where(article_type: "Social").take(5)
   end
 
   def sports
     @page = "sports"
     @subscription = Subscription.new
-    @articles = Article.where(article_type: "Sports").sort_by! { |a| [a.isOld ? 1 : 0, -a.get_upvotes.size, -a.views, -a.created_at.to_i, -a.id]}.paginate(:page => params[:page], :per_page => 20)
-    @top_articles = Article.where(article_type: "Sports").sort_by! { |a| [-a.get_upvotes.size, -a.views, -a.id]}.take(5)
+    @articles = Article.where(article_type: "Sports").paginate(:page => params[:page], :per_page => 20)
+    @top_articles = Article.where(article_type: "Sports").take(5)
   end
 
   def random
     @page = "random"
     @subscription = Subscription.new
-    @articles = Article.where(article_type: "Random").sort_by! { |a| [a.isOld ? 1 : 0, -a.get_upvotes.size, -a.views, -a.created_at.to_i, -a.id]}.paginate(:page => params[:page], :per_page => 20)
-    @top_articles = Article.where(article_type: "Random").sort_by! { |a| [-a.get_upvotes.size, -a.views, -a.id]}.take(5)
+    @articles = Article.where(article_type: "Random").paginate(:page => params[:page], :per_page => 20)
+    @top_articles = Article.where(article_type: "Random").take(5)
   end
 
   def about
